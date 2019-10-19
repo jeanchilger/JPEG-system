0a3908d5890eaab0a1105e59bb11a35d2c57b7c6
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2019-10-19 19:00:05 -0300

    adds forms and colors

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title> JPEG Eventos </title>
+
+        <link rel="stylesheet" href="frontend/assets/css/index.css">
+    </head>
+
+    <body>
+        <p>Primary btn:</p>
+        <a class="btn btn-primary" href="#">button</a>
+        <br>
+
+        <p>Secondary btn:</p>
+        <a class="btn btn-secondary" href="#">button</a>
+        <br>
+
+        <p>Primary gradient btn:</p>
+        <a class="btn btn-primary--gradient" href="#">button</a>
+        <br>
+
+        <p>Secondary gradient btn:</p>
+        <a class="btn btn-secondary--gradient" href="#">button</a>
+        <br>
+
+        <form class="" action="index.html" method="post">
+            <p>INPUT TYPE TEXT PRIMARY</p>
+
+            <div class="form-group form-group--primary">
+                <input type="text" id="ex1" value="">
+                <label for="ex1">username</label>
+            </div>
+
+            <p>INPUT TYPE TEXT SECONDARY</p>
+
+            <div class="form-group form-group--secondary">
+                <input type="text" id="ex1" value="">
+                <label for="ex1">username</label>
+            </div>
+        </form>
+    </body>
+</html>
