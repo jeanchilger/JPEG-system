c4c11f353a376a0d11e249eaff4c99b17398123a
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2019-11-18 12:58:39 -0300

    adds project in laravel framework

---
+++ jpeg/resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>JPEG Eventos</title>
+
+        <link rel="stylesheet" href="../css/index.css">
+    </head>
+
+    <body>
+        @yield("content")
+
+    </body>
+</html>
